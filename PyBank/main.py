d69977128c184b5d8a7433f0099f65cacdf07832
Samuel Heaton <itsmesamnit@gmail.com>
2022-09-29 10:02:04 -0500

    PyBank Complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,37 @@
-import os   # This will allow us to create file paths across operating systems
+import os   # Allow us to create file paths across op systems
 import csv  # Module for reading CSV files
 
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+months = 0
+total_pl = 0
+max_increase = 0
+max_decrease = 0
 
-with open(csvpath) as csvfile:
+csvpath = os.path.join(".", "Resources", "budget_data.csv")
+
+with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)         #go past header
+    for row in csvreader:
+        months += 1
+        pl = int(row[1])    # pl = Profit/Losses
+        total_pl += pl
+        if pl > max_increase :
+            max_increase = pl
+            max_increase_date = row[0]
+        if pl < max_decrease :
+            max_decrease = pl
+            max_decrease_date = row[0]
 
-    print(csvreader)
-    #print(f"Total number of months included in the dataset: {len(csvreader)}")
+report = ("Financial Analysis\n--------------------------------\n\
+Total Months: %d\nTotal: %d\nAverage Change: $%.2f\n\
+Greatest Increase in Profits: %s ($%d)\n\
+Greatest Decrease in Profits: %s ($%d)"\
+% (months, total_pl, total_pl/months, max_increase_date, \
+max_increase, max_decrease_date, max_decrease))
+
+print(report)
+
+output_path = os.path.join(".", "Analysis", "Analysis.txt")
+
+with open(output_path, 'w') as f:
+    f.write(report)
