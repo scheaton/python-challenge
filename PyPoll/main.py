34384b562664c236afed9873f4927d8c40bbf908
Samuel Heaton <112985362+scheaton@users.noreply.github.com>
2022-10-02 20:13:18 -0500

    Added comments for ease of reading.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,18 +1,19 @@
 import os   # Allow us to create file paths across op systems
 import csv  # Module for reading CSV files
 
-candidates = []
-candidate_i = 0
-votes = []
+candidates = [] # list of all unique candidates
+votes = []      # list of all unique candidates vote totals
+candidate_i = 0 # indexing used in candidates[] and votes[]
+# eg. candidates[candidate_i]'s vote total is votes[candidate_i]
 
 csvpath = os.path.join(".", "Resources", "election_data.csv")
 
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    header = next(csvreader)    # save and go past header
+    header = next(csvreader)    # Save and go past header.
     for row in csvreader:
-        if row[2] not in candidates :
-            candidates.append(row[2])
+        if row[2] not in candidates :   # If there is a new candidate then
+            candidates.append(row[2])   # add them to the candidates list.
             votes.append(0)
         candidate_i = candidates.index(row[2])
         votes[candidate_i] += 1
@@ -22,8 +23,8 @@ winner = candidates[votes.index(max(votes))]
 
 print("'''text\nElection Results\n-------------------------\n\
 Total Votes: %d\n-------------------------" % total_vote_count)
-for i in range(len(candidates)):
-    print("%s: %.3f%% (%d)" \
+for i in range(len(candidates)):    # Itereate statement for
+    print("%s: %.3f%% (%d)" \       # each unique candidate.
     % (candidates[i], votes[i]*100/total_vote_count, votes[i]))
 print("-------------------------\nWinner: %s\n\
 -------------------------\n'''" % winner)
